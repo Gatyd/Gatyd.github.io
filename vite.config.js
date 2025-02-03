1eccd013ab3457a601670ce0b70c42fadb02b5c6
Gatyd <devgatyd@gmail.com>
2025-02-03 19:12:00 +0100

    Mise à jour de la configuration pour déployer sur GitHub Pages et ajout de gh-pages dans les dépendances

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/Gatyd.github.io/',
   plugins: [
     vue(),
     vueDevTools()
